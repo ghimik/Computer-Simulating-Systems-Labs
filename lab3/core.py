1e2f02905d29849a62d31c1440ee05802e01093c
ghimik <bolit006.br0m@gmail.com>
2025-02-04 23:26:47 +0300

    Implement core functionality for function analysis and extremum finding

---
+++ lab3/core.py
@@ -0,0 +1,307 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from abc import ABC, abstractmethod
+from typing import List, Optional, Tuple, Callable
+
+class Function(ABC, Callable[[float], float]):
+    @abstractmethod
+    def value_at(self, x: float) -> float:
+        pass
+
+    def __call__(self, x: float) -> float:
+        return self.value_at(x)
+
+    def __str__(self) -> str:
+        return "f(x)"
+
+    def derivative(self) -> 'Function':
+        return NumericalDerivative(self)
+
+class LaboratoryFunction(Function):
+    def value_at(self, x: float) -> float:
+        return x * math.exp(x) * (math.sin(x))**2
+
+    def __str__(self) -> str:
+        return "f(x) = x * e^x * (sin x)^2"
+
+    def derivative(self) -> 'Function':
+        return CustomStringAndLambdaFunction("f'(x) = e^x * (sin x)^2 + 2x * sin x * cos x + x * (sin x)^2", 
+                                             lambda x: math.exp(x) * (math.sin(x)**2 + 2*x*math.sin(x)*math.cos(x) + x*math.sin(x)**2))
+
+class CustomLambdaFunction(Function):
+    def __init__(self, func: Callable[[float], float]) -> None:
+        self.func = func
+
+    def value_at(self, x: float) -> float:
+        return self.func(x)
+
+class CustomStringFunction(Function):
+    def __init__(self, expression: str) -> None:
+        self.expression = expression
+
+    def value_at(self, x: float) -> float:
+        return eval(self.expression, {"x": x, "math": math})
+
+    def __str__(self) -> str:
+        return self.expression
+    
+class CustomStringAndLambdaFunction(Function):
+    def __init__(self, expression: str, lambda_func: Callable[[float], float]) -> None:
+        self.expression = expression
+        self.lambda_func = lambda_func
+
+    def value_at(self, x: float) -> float:
+        return self.lambda_func(x)
+
+    def __str__(self) -> str:
+        return self.expression
+
+class NumericalDerivative(Function):
+    def __init__(self, function: Function, h: float = 1e-5) -> None:
+        self.function = function
+        self.h = h
+
+    def value_at(self, x: float) -> float:
+        h = max(self.h, abs(x) * 1e-5)
+        return (self.function.value_at(x + h) - self.function.value_at(x - h)) / (2 * h)
+
+    def __str__(self) -> str:
+        return f"d({self.function})/dx"
+
+class Functions:
+    @staticmethod
+    def lab_function() -> Function:
+        return LaboratoryFunction()
+
+    @staticmethod
+    def new_from_lambda(lambda_func: Callable[[float], float]) -> Function:
+        return CustomLambdaFunction(lambda_func)
+
+    @staticmethod
+    def new_from_string(expression: str) -> Function:
+        return CustomStringFunction(expression)
+
+class Interval:
+    def __init__(self, start: float, end: float) -> None:
+        self._start = start
+        self._end = end
+
+    def from_(self) -> float:
+        return self._start
+
+    def to(self) -> float:
+        return self._end
+
+    def traverse(self, mapper: Callable[[float], float] = None, step: float = 1e-7) -> List[float]:
+        x = self._start
+        results = []
+        while x <= self._end:
+            if mapper is None:
+                results.append(x)
+            else:
+                results.append(mapper(x))
+            x += step
+        return results
+
+class GraphBuilder:
+    def __init__(self, function: Function, interval: Interval, step: float = 0.01) -> None:
+        self.function = function
+        self.interval = interval
+        self.step = step
+
+    def build(self) -> None:
+        x = self.interval.traverse(mapper=None, step=self.step)
+        y = self.interval.traverse(self.function, self.step)
+        plt.plot(x, y)
+        plt.xlabel('x')
+        plt.ylabel('f(x)')
+        plt.title(f'График функции {self.function}')
+        plt.grid()
+        plt.show()
+
+
+class Extremum:
+    def __init__(self, point: float, value: float) -> None:
+        self._point = point
+        self._value = value
+
+    @property
+    def point(self) -> float:
+        return self._point
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+    def __str__(self) -> str:
+        return f"Extremum at x = {self.point}, value = {self.value}"
+
+class Minimum(Extremum):
+    def __str__(self) -> str:
+        return f"Minimum at x = {self.point}, value = {self.value}"
+
+class Maximum(Extremum):
+    def __str__(self) -> str:
+        return f"Maximum at x = {self.point}, value = {self.value}"
+
+class ExtremumFinder(ABC):
+    @abstractmethod
+    def find_extremum(self, function: Function, interval: Interval, epsilon: float = 1e-7) -> Optional[Extremum]:
+        pass
+
+class IterationalContext(ABC):
+    def __init__(self):
+        self.iteration_count = 0
+
+    @abstractmethod
+    def count_iteration(self):
+        pass
+
+    def drop_iterations(self):
+        self.iteration_count = 0
+
+    def get_iteration_count(self) -> int:
+        return self.iteration_count
+
+class UnlimitedIterationalContext(IterationalContext):
+    def count_iteration(self):
+        self.iteration_count += 1
+
+class LimitedIterationalContext(IterationalContext):
+    def __init__(self, max_iterations: int):
+        super().__init__()
+        self.max_iterations = max_iterations
+
+    def count_iteration(self):
+        if self.iteration_count >= self.max_iterations:
+            raise Exception("Maximum number of iterations exceeded")
+        self.iteration_count += 1
+
+class IterationalExtremumFinder(ExtremumFinder):
+    def __init__(self, context: IterationalContext):
+        self.context = context
+
+    def count_iteration(self):
+        self.context.count_iteration()
+
+    def drop_iterations(self):
+        self.context.drop_iterations()
+
+class DichotomyMethod(IterationalExtremumFinder):
+
+    def __init__(self, context: IterationalContext):
+        super().__init__(context)
+
+    def find_extremum(self, function: Function, interval: Interval, epsilon: float = 1e-7) -> Optional[Extremum]:
+        a, b = interval.from_(), interval.to()
+        derivative = function.derivative()
+        
+        if derivative.value_at(a) * derivative.value_at(b) > 0:
+            raise Exception("No extremum in the interval")
+        
+        is_minimum = True
+        while (b - a) / 2 > epsilon:
+            self.count_iteration()
+            x1 = (b + a - epsilon) / 2
+            x2 = (b + a + epsilon) / 2
+            if function.value_at(x1) <= function.value_at(x2):
+                b = x2
+                is_minimum = True
+            else:
+                a = x1
+                is_minimum = False
+            
+        point = (a + b) / 2
+        if is_minimum:
+            return Minimum(point, function.value_at(point))
+        else:
+            return Maximum(point, function.value_at(point))
+
+class GoldenSectionMethod(IterationalExtremumFinder):
+    golden_section_tau: float = (math.sqrt(5) - 1) / 2
+
+    def __init__(self, context: IterationalContext):
+        super().__init__(context)
+
+    def find_extremum(self, function: Function, interval: Interval, epsilon: float = 1e-7) -> Optional[Extremum]:
+        a, b = interval.from_(), interval.to()
+        derivative = function.derivative()
+        
+        if derivative.value_at(a) * derivative.value_at(b) > 0:
+            raise Exception("No extremum in the interval")
+
+        lambda1 = a + (1 - self.golden_section_tau) * (b - a)
+        mu1 = a + self.golden_section_tau * (b - a)
+        is_minimum = True
+        while abs(b - a) > epsilon:
+            self.count_iteration()
+            if function.value_at(lambda1) < function.value_at(mu1):
+                b = mu1
+                mu1 = lambda1
+                lambda1 = a + (1 - self.golden_section_tau) * (b - a)
+                is_minimum = True
+            else:
+                a = lambda1
+                lambda1 = mu1
+                mu1 = a + self.golden_section_tau * (b - a)
+                is_minimum = False
+
+        point = (a + b) / 2
+        if is_minimum:
+            return Minimum(point, function.value_at(point))
+        else:
+            return Maximum(point, function.value_at(point))
+
+class Optional[T]:
+    def __init__(self, value: T = None):
+        self._value = value
+
+    def is_present(self) -> bool:
+        return self._value is not None
+
+    def get(self) -> T:
+        if self._value is None:
+            raise ValueError("No value present")
+        return self._value
+
+    def or_else(self, other: T) -> T:
+        return self._value if self._value is not None else other
+
+    def __str__(self):
+        return f"Optional({self._value})"
+    
+
+class ExtremumIntervalDetector:
+    def __init__(self, function: Function, step: float = None, search_interval: Interval = Interval(-1000, 1000)):
+        self.function = function
+        self.search_interval = search_interval
+        if step is None:
+            if search_interval is not None:
+                self.step = (search_interval.to() - search_interval.from_()) / 1000
+            else:
+                self.step = 0.01
+        else:
+            self.step = step
+
+    def find_extremum_intervals(self) -> List[Interval]:
+        derivative = self.function.derivative()
+        x_values = np.arange(self.search_interval.from_(), self.search_interval.to(), self.step)
+        previous_value = derivative.value_at(x_values[0])
+        potential_intervals = []
+
+        for i in range(1, len(x_values) - 1):
+            current_value = derivative.value_at(x_values[i])
+            next_value = derivative.value_at(x_values[i + 1])
+
+            # смена знака производной
+            if (previous_value * current_value < 0) or (current_value * next_value < 0):
+                potential_intervals.append(Interval(x_values[i - 1], x_values[i + 1]))
+
+            # проверкана близость к нулю
+            if abs(current_value) < self.step:  
+                potential_intervals.append(Interval(x_values[i - 1], x_values[i + 1]))
+
+            previous_value = current_value
+
+        return potential_intervals
---
+++ lab3/main.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+from core import *
+
+
+
+def script_one():
+    function = Functions.lab_function()
+    derivative = function.derivative()
+    interval = Interval(-2, 2)
+
+    graph_function = GraphBuilder(function, interval, 0.01)
+    graph_derivative = GraphBuilder(derivative, interval, 0.01)
+
+    graph_function.build()
+    graph_derivative.build()
+
+def script_two():
+    function = Functions.lab_function()
+    interval = Interval(-2, 2)
+    context = LimitedIterationalContext(max_iterations=50000)
+    dichotomy = DichotomyMethod(context)
+
+    try:
+        extremum = dichotomy.find_extremum(function, interval)
+        print(extremum, context.get_iteration_count())
+    except Exception as e:
+        print(e)
+
+def script_three():
+    function = Functions.new_from_lambda(lambda x: x * (math.sin(x))**2)
+    interval = Interval(-2, 2)
+    context = LimitedIterationalContext(max_iterations=50000)
+    golden_section = GoldenSectionMethod(context)
+
+    try:
+        extremum = golden_section.find_extremum(function, interval)
+        print(extremum, context.get_iteration_count())
+    except Exception as e:
+        print(e)
+
+def script_four():
+    function = Functions.new_from_string("math.sin(x**2)")
+    detector = ExtremumIntervalDetector(function)
+    context = UnlimitedIterationalContext()
+    golden_section = GoldenSectionMethod(context)
+
+    extremum_intervals = detector.find_extremum_intervals()
+    for interval in extremum_intervals:
+        print(f"Extremum interval: from {interval.from_()} to {interval.to()}")
+        try:
+            extremum = golden_section.find_extremum(function, interval)
+            print(extremum, context.get_iteration_count())
+        except Exception as e:
+            print(e)
+
+def main():
+    function = Functions.lab_function()
+    search_interval = Interval(0, 20)
+    finder = ExtremumIntervalDetector(function, search_interval=search_interval)
+    intervals = finder.find_extremum_intervals()
+    graph_function = GraphBuilder(function, search_interval, 0.01)
+    graph_function.build()
+
+    epsilons = [1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12, 1e-13]
+
+    golden_context = LimitedIterationalContext(max_iterations=200_000)
+    dichotomy_context = LimitedIterationalContext(max_iterations=200_000)
+
+    golden_method = GoldenSectionMethod(golden_context)
+    dichotomy_method = DichotomyMethod(dichotomy_context)
+
+    print(f"{'Точность':<10} {'Золотое сечение':<20} {'Дихотомия':<10}")
+    for epsilon in epsilons:
+        golden_context.drop_iterations()
+        dichotomy_context.drop_iterations()
+        
+        for interval in intervals:
+            try:
+                golden_method.find_extremum(function, interval, epsilon)
+            except Exception as e:
+                pass
+            
+            try:
+                dichotomy_method.find_extremum(function, interval, epsilon)
+            except Exception as e:
+                pass
+        
+        print(f"{epsilon:<10} {golden_context.get_iteration_count():<20} {dichotomy_context.get_iteration_count():<10}")
+
+
+if __name__ == "__main__":
+    main()
